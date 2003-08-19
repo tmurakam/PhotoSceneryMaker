1b5a4dd1cb40fb1221c2d295a26c786fbe9d823b
tmurakam <tmurakam@6c73a3f0-0c1e-0410-8739-a278547e6925>
2003-08-19 16:30:43 +0000

    translate
    git-svn-id: https://svn.code.sf.net/p/scenerytools/code/trunk/PhotoSceneryMaker@65 6c73a3f0-0c1e-0410-8739-a278547e6925

--- Util.cpp
+++ Util.cpp
@@ -45,30 +45,31 @@ static int CALLBACK BrowseCallbackProc(HWND hwnd,UINT uMsg,LPARAM lParam,LPARAM 
 
 
 AnsiString SelectFolder(
-	AnsiString InitFolder,	// 初期フォルダ
-	HWND owner)              //オーナーウインドウのハンドル
+	AnsiString InitFolder,	// Initial Folder
+	HWND owner)              // Owner window handle
 {
   AnsiString s;
-  IMalloc *m; //タスクアロケータ
+  IMalloc *m; // Task allocator
 
   if(SUCCEEDED(SHGetMalloc(&m)))
   {
     BROWSEINFO b;
     char path[MAX_PATH];
 
-    b.hwndOwner= owner; //オーナーウィンドウ
-    b.pidlRoot= NULL;   //ルート
-    b.pszDisplayName= path; //選択したものの表示名格納場所
-    b.lpszTitle= "フォルダを選んで下さい"; //表示メッセージ
-    b.ulFlags= BIF_RETURNONLYFSDIRS; //フォルダのみ選べる
-    b.lpfn= BrowseCallbackProc; //コールバック関数
-    b.lParam= (LPARAM)InitFolder.c_str();  //コールバック関数に渡される値
-    b.iImage= 0;  //選択したもののイメージ番号
-  //ダイアログの表示
+    b.hwndOwner= owner; // Owner handle
+    b.pidlRoot= NULL;   // Root
+    b.pszDisplayName= path; // Display name buffer
+    b.lpszTitle= "Select folder"; // Message
+    b.ulFlags= BIF_RETURNONLYFSDIRS; // folder only
+    b.lpfn= BrowseCallbackProc; // callback func
+    b.lParam= (LPARAM)InitFolder.c_str();  // Callback value
+    b.iImage= 0;  // image index
+
+    // Show dialog
     LPITEMIDLIST id=SHBrowseForFolder(&b);
     if(id)
     {
-    //パス名を得る
+      // Get path
       SHGetPathFromIDList(id, path);
       s= path;
       m->Free(id);
