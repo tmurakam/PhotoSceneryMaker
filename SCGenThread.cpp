97d1a2ae1402711c5eb09fac165b75fc5c9aa1a6
tmurakam <tmurakam@6c73a3f0-0c1e-0410-8739-a278547e6925>
2003-08-20 16:43:18 +0000

    initial
    git-svn-id: https://svn.code.sf.net/p/scenerytools/code/trunk/PhotoSceneryMaker@71 6c73a3f0-0c1e-0410-8739-a278547e6925

---
+++ SCGenThread.cpp
@@ -0,0 +1,330 @@
+/*
+ * Photo Scenery Maker
+ *
+ * Copyright (C) 2003 Takuya Murakami
+ *
+ * SCGenFrm.cpp : Scenery generation thread
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <stdio.h>
+
+#include "gnugettext.hpp"
+
+#include "SCGenThread.h"
+#include "OptDlg.h"
+#include "targa.h"
+
+#pragma package(smart_init)
+
+//---------------------------------------------------------------------------
+const char *SeasonSuffix[] = SEASON_SUFFIX;
+const char *SeasonName[] = SEASON_NAME;
+
+//---------------------------------------------------------------------------
+
+// 注意：異なるスレッドが所有する VCL のメソッド/関数/プロパティを
+//       別のスレッドの中から扱う場合，排他処理の問題が発生します。
+//
+//       メインスレッドの所有するオブジェクトに対しては Synchronize
+//       メソッドを使う事ができます。他のオブジェクトを参照するため
+//       のメソッドをスレッドクラスに追加し，Synchronize メソッドの
+//       引数として渡します。
+//
+//       たとえば UpdateCaption を以下のように定義し、
+//
+//      void __fastcall SCGenThread::UpdateCaption()
+//      {
+//        Form1->Caption = "スレッドから書き換えました";
+//      }
+//
+//       Execute メソッドの中で Synchronize メソッドに渡すことでメイ
+//       ンスレッドが所有する Form1 の Caption プロパティを安全に変
+//       更できます。
+//
+//      Synchronize(UpdateCaption);
+//
+//---------------------------------------------------------------------------
+
+__fastcall SCGenThread::SCGenThread(PSMProject *proj, int sw)
+	: TThread(true)
+{
+	Sw = sw;
+	Proj = proj;
+}
+//---------------------------------------------------------------------------
+void __fastcall SCGenThread::Execute()
+{
+	if (Sw & EX_MAKEINF) {
+		MakeInf();
+	}
+	if (Sw & EX_RESAMPLE) {
+		Resample();
+	}
+	if (Sw & EX_MRGALPHA) {
+		MergeAlpha();
+	}
+	if (Sw & EX_CONVTEX) {
+		ConvTex();
+	}
+	if (Sw & EX_GENBGL) {
+		GenBgl();
+	}
+}
+
+//---------------------------------------------------------------------------
+// INF ファイル作成
+void SCGenThread::MakeInf(void)
+{
+	// 出力ディレクトリを作成
+	MkDir(Proj->OutDir);
+	MkDir(Proj->OutDir + "\\TmpBmp");
+	MkDir(Proj->OutDir + "\\TmpAlpha");
+
+	if (!Proj->HasSeason) {
+		MakeInf(BM_SUMMER);
+		MakeInf(BM_ALPHA);
+	} else {
+		for (int i = 0; i < BM_MAX; i++) {
+			MakeInf(i);
+		}
+	}
+}
+
+//---------------------------------------------------------------------------
+// INF ファイル名決定
+AnsiString SCGenThread::InfFileName(int season)
+{
+	AnsiString path = Proj->OutDir;
+	path.sprintf("%s\\%s.inf", Proj->OutDir, SeasonSuffix[season]);
+
+	return path;
+}
+
+// BMP パス決定
+AnsiString SCGenThread::BmpPath(int season)
+{
+	AnsiString path;
+	if (season == BM_ALPHA) {
+		path.sprintf("%s\\TmpAlpha", Proj->OutDir);
+	} else {
+		path.sprintf("%s\\TmpBmp", Proj->OutDir);
+	}
+	return path;
+}
+
+//---------------------------------------------------------------------------
+// 一個の INF ファイルを生成する
+void SCGenThread::MakeInf(int season)
+{
+	AnsiString inf = InfFileName(season);
+
+	AnsiString BmpFile = Proj->BmpFile(season);
+	if (BmpFile.IsEmpty()) {
+		return;
+	}
+
+	// inf ファイルを作成
+	FILE *fp;
+	fp = fopen(inf.c_str(), "w");
+
+	Transform *trans = Proj->Trans;
+
+	// Source セクション
+	fprintf(fp, "[Source]\n");
+	fprintf(fp, "\tType = CUSTOM\n");
+	fprintf(fp, "\tSourceDir = \"%s\"\n", ExtractFileDir(BmpFile).c_str());
+	fprintf(fp, "\tSourceFile = \"%s\"\n", ExtractFileName(BmpFile).c_str());
+	fprintf(fp, "\tLat = %.24f\n", trans->Base.lat.deg);
+	fprintf(fp, "\tLon = %.24f\n", trans->Base.lon.deg);
+	fprintf(fp, "\tNumOfCellsPerLine = %d\n", trans->Width);
+	fprintf(fp, "\tNumOfLines = %d\n", trans->Height);
+	fprintf(fp, "\tCellXdimensionDeg = %.24f\n", trans->Resolution.x);
+	fprintf(fp, "\tCellYdimensionDeg = %.24f\n", trans->Resolution.y);
+
+	// Destination
+	fprintf(fp, "\n[Destination]\n");
+	fprintf(fp, "\tDestDir = \"%s\"\n", BmpPath(season).c_str());
+	fprintf(fp, "\tDestBaseFileName = \"%s\"\n", Proj->BaseFile.c_str());
+	fprintf(fp, "\tUseSourceDimensions = 1\n");
+	if (Proj->HasSeason) {
+		fprintf(fp, "\twithseasons = 1\n");
+		fprintf(fp, "\tseason = %s\n", SeasonName[season]);
+	}
+		
+	fclose(fp);
+}
+
+int SCGenThread::ExecCmd(AnsiString cmdline)
+{
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
+
+	memset(&si, 0, sizeof(si));
+	si.cb=sizeof(si);
+
+	CreateProcess(NULL, cmdline.c_str(),NULL,NULL,FALSE,
+		BELOW_NORMAL_PRIORITY_CLASS,
+		NULL,NULL,&si,&pi);
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	CloseHandle(pi.hProcess);
+
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+// resample 実行
+void SCGenThread::Resample(void)
+{
+	for (int i = 0; i < BM_MAX; i++) {
+		if (!Proj->HasSeason &&
+		    i != BM_SUMMER && i != BM_ALPHA) {
+			continue;
+		}
+
+		// 作業用ディレクトリにchdir する
+		AnsiString bmppath = BmpPath(i);
+		ChDir(bmppath);
+
+		// resample を実行する
+		AnsiString inf = InfFileName(i);
+	
+		AnsiString cmdline;
+		cmdline.sprintf("%s\\resample.exe %s",
+				OptionDlg->GetSDKPath(), inf);
+		if (ExecCmd(cmdline) != 0) {
+			AnsiString title = _("Error");
+			AnsiString mes = _("Some errors occured.");
+			Application->MessageBox(mes.c_str(), title.c_str(), MB_OK);
+		}
+	}
+
+	// tmf ファイルを移動する
+	AnsiString tmf;
+	tmf.sprintf("%s\\%s.tmf", BmpPath(BM_SUMMER), Proj->BaseFile);
+	AnsiString newtmf;
+	newtmf.sprintf("%s\\%s.tmf", Proj->OutDir, Proj->BaseFile);
+
+	DeleteFile(newtmf.c_str());
+	MoveFile(tmf.c_str(), newtmf.c_str());
+
+	// いらないファイルを削除
+	tmf.sprintf("%s\\%s.tmf", BmpPath(BM_ALPHA), Proj->BaseFile);
+	DeleteFile(tmf.c_str());
+}
+//---------------------------------------------------------------------------
+// Alpha テクスチャのマージ
+void SCGenThread::MergeAlpha(void)
+{
+	// texture ディレクトリ生成
+	AnsiString texdir;
+	texdir.sprintf("%s\\texture", Proj->OutDir);
+	MkDir(texdir);
+
+	// Targa ファイルを生成する
+	AnsiString bmpdir = BmpPath(BM_SUMMER);
+	AnsiString alphadir = BmpPath(BM_ALPHA);
+
+	AnsiString searchfile;
+	searchfile.sprintf("%s\\*su.bmp", bmpdir);
+
+	AnsiString alphafile, bmpfiles[BM_MAX], tgafiles[BM_MAX], pre;
+
+	TSearchRec rec;
+	int ret = FindFirst(searchfile, faAnyFile & ~faDirectory, rec);
+	int count = 1;
+	while (ret == 0) {
+
+#if 1
+		// TBD: メインスレッドに進捗報告
+#else
+		AnsiString st;
+		st.sprintf("Merging textures : %d", count++);
+		StatusBar->SimpleText = st;
+		Application->ProcessMessages();
+#endif
+
+		// ファイル名の最初の部分
+		int len = rec.Name.Length();
+		pre = rec.Name.SubString(1, len - 6);
+
+		// alpha テクスチャ名
+		alphafile.sprintf("%s\\%ssu.bmp", alphadir, pre);
+
+		// bmp ファイル名
+		for (int i = 0; i < BM_MAX; i++) {
+			if (i == BM_ALPHA) continue;
+			bmpfiles[i].sprintf("%s\\%s%s.bmp", bmpdir, pre, SeasonSuffix[i]);
+			tgafiles[i].sprintf("%s\\%s%s.tga", texdir, pre, SeasonSuffix[i]);
+		}
+		MergeAlphaTextures(bmpfiles, alphafile, tgafiles);
+
+		ret = FindNext(rec);
+	}
+	FindClose(rec);
+}
+
+//---------------------------------------------------------------------------
+// テクスチャ変換
+void SCGenThread::ConvTex(void)
+{
+	AnsiString Imagetool;
+	Imagetool.sprintf("%s\\imagetool.exe", OptionDlg->GetImagetoolPath());
+
+	AnsiString cmdline;
+	cmdline.sprintf("\"%s\" -DXT1 -e bmp -terrainphoto \"%s\\texture\\*.tga\"",
+		Imagetool, Proj->OutDir);
+
+	ExecCmd(cmdline);
+}
+//---------------------------------------------------------------------------
+// BGL 生成
+void SCGenThread::GenBgl(void)
+{
+	AnsiString outdir = Proj->OutDir;
+	AnsiString basefile = Proj->BaseFile;
+
+	// BGL 出力ディレクトリ生成
+	AnsiString scdir;
+	scdir.sprintf("%s\\scenery", outdir);
+	MkDir(scdir);
+
+	// BGL ファイル名生成
+	AnsiString bgl;
+	bgl.sprintf("%s\\scenery\\%s.bgl", outdir, basefile);
+
+	// 中間ファイル名生成
+	AnsiString tmf, tmfc;
+	tmf.sprintf("%s\\%s.tmf", outdir, basefile);
+	tmfc.sprintf("%s\\%s-cmp.tmf", outdir, basefile);
+
+	AnsiString sdkpath;
+	sdkpath = OptionDlg->GetSDKPath();
+
+	// compress する
+	AnsiString cmdline;
+	cmdline.sprintf("%s\\tmfcompress.exe %s %s", sdkpath, tmf, tmfc);
+	ExecCmd(cmdline);
+
+	// BGL に変換する
+	cmdline.sprintf("%s\\tmf2bgl.exe %s %s", sdkpath, tmfc, bgl);
+	ExecCmd(cmdline);
+}
---
+++ SCGenThread.h
@@ -0,0 +1,42 @@
+//---------------------------------------------------------------------------
+
+#ifndef SCGenThreadH
+#define SCGenThreadH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+
+#include "Project.h"
+
+#define	EX_MAKEINF	0x1
+#define	EX_RESAMPLE	0x2
+#define	EX_MRGALPHA	0x4
+#define	EX_CONVTEX	0x8
+#define	EX_GENBGL	0x10
+
+//---------------------------------------------------------------------------
+class SCGenThread : public TThread
+{            
+private:
+protected:
+	void __fastcall Execute();
+
+	PSMProject *Proj;
+	int Sw;
+
+	void MakeInf(void);
+	AnsiString InfFileName(int season);
+	AnsiString BmpPath(int season);
+	void MakeInf(int season);
+
+	int ExecCmd(AnsiString cmdline);
+
+	void Resample(void);
+	void MergeAlpha(void);
+	void ConvTex(void);
+	void GenBgl(void);
+
+public:
+	__fastcall SCGenThread(PSMProject *proj, int sw);
+};
+//---------------------------------------------------------------------------
+#endif
